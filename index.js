9c0bb10bca5b606d0cbc771179f373bc55dfbeee
DjJewl <47831173+wolflord212312@users.noreply.github.com>
2022-01-25 11:25:51 -0600

    FIxes

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const token = config.token;
 const Ref = config.reflink;
 const colors = require('colors');
 const { message } = require('prompt');
-
+const guilds = config.guildID
+const channels = config.channelID
 ///////////////////////
 ///  DOING CHECKS   ///
 ///////////////////////
 const interval = config.time * 1000;
-let guild = client.guilds.cache.get(config.guildID);
+let guild = client.guilds.cache.get(`${guilds}`);
 client.destroy() //logs out of Discord
 ///////////////////////
 ///  LOADING CLIENT ///
@@ -48,13 +49,12 @@ headers: {
 
 axios(config)
 .then(function (response) {
-    let count = 0;
     var data = response.data; //get data from response
     let getMB = data.gathering.bytes / Math.pow(1024, 2); //convert to MB
     let mbs = getMB.toFixed(2); //round to 2 decimal places   
-      const channel = client.channels.cache.get(config.channelID); // Do nothing if the channel wasn't found on this server 
+      const channel = client.channels.cache.get(`${channels}`); // Do nothing if the channel wasn't found on this server 
       if (!channel) return; // Send the message, mentioning the member 
-      let embed = new discord.MessageEmbed() //create embed
+      const embed = new discord.MessageEmbed() //create embed
     .setTitle('HoneyGain status')
     .setImage('https://bit.ly/')
     .setColor('RANDOM')
@@ -68,8 +68,6 @@ axios(config)
     .setTimestamp();
     channel.send({ embeds: [embed] });
     console.log(`[HG Bot] New Update! \n    [HG Bot] Earned today: ${data.total.credits} \n    [HG Bot] Gathering:  ${mbs} MB - ${data.gathering.credits} CR \n    [HG Bot] Winnings: ${data.winning.credits}`.blue); //log to console
-
-    
 })
 .catch(function (error) {
 console.log(error);
